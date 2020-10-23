25cb6de2a8c64a7d245c2ee084d5bfb1c44907c3
Kurtil <gaetan.lagier@hotmail.fr>
2020-10-23 18:01:39 +0200

    add basic BIMData routing
    API / Platform / Viewer

---
+++ src/.vuepress/components/BIMDataHome.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <section>
+      <div>
+        <div class="home_illustration">
+          <img :src="$withBase('/assets/img/home_illu.svg')" alt="" />
+        </div>
+        <div class="home_links">
+          <a href="api">API</a>
+          <a href="platform">Platform</a>
+          <a href="viewer">Viewer</a>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style>
+.home_illustration {
+  text-align: center;
+}
+
+.home_links {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ src/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: "Documentation",
+  head: [
+    ['link', { rel: 'icon', href: '/assets/img/favicon.svg', type: "image/svg+xml" }]
+  ],
+  themeConfig: {
+    logo: '/assets/img/logo.svg',
+    nav: [
+      { text: 'API', link: '/api/' },
+      { text: 'Platform', link: '/platform/' },
+      { text: 'Viewer', link: '/viewer/' },
+      { text: 'Github', link: 'https://github.com/bimdata/documentation-next' }
+    ]
+  }
+}
