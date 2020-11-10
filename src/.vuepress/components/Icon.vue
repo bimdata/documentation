120b07863f8d6aaa65b2d94dae2fced4cd3d0e03
Kurtil <gaetan.lagier@hotmail.fr>
2020-11-10 16:36:54 +0100

    add Icon component binded to DS BIMDataIcon

---
+++ src/.vuepress/components/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+  <BIMDataIcon :name="name" :size="size" />
+</template>
+
+<script>
+import { BIMDataIcon } from "@bimdata/design-system"
+
+export default {
+  components: {
+    BIMDataIcon
+  },
+  props: {
+    name: String,
+    size: String,
+  }
+}
+</script>
+
+<style>
+
+</style>
