ecc1ae729857198839150e80879383a83206ce36
NicolasRichel <nicolas@bimdata.io>
2024-01-29 10:31:08 +0100

    feat(viewer): add cookbook section with plan annotations recipe

---
+++ src/.vuepress/viewer/config/planAnnotations.js
@@ -0,0 +1,134 @@
+const PlanAnnotationsPlugin = {
+  name: "planAnnotations",
+  addToWindows: ["plan"],
+  button: {
+    position: "right",
+    keepOpen: true,
+    tooltip: "planAnnotations.tooltip",
+    icon: {
+      component: "BIMDataIcon",
+      options: { name: "location", size: "m" },
+    },
+  },
+  i18n: {
+    fr: {
+      tooltip: "Annotations PDF",
+    },
+  },
+  component: {
+    data() {
+      return {
+        index: 0
+      };
+    },
+    render() {
+      return null;
+    },
+    onOpen() {
+      const state = this.$viewer.state;
+      const context = this.$viewer.localContext;
+      context.startAnnotationMode(({ x, y }) => {
+        const annotation = state.addAnnotation({
+          component: PlanAnnotation,
+          props: {
+            index: ++this.index,
+            moveTo: position => Object.assign(annotation, position),
+            remove: () => state.removeAnnotation(annotation),
+          },
+          x,
+          y,
+          z: 0,
+        });
+        context.stopAnnotationMode();
+        this.$close();
+      });
+    },
+  },
+};
+
+const PlanAnnotation = {
+  template: `
+    <div
+      class="plan-annotation"
+      :class="{ grabbing }"
+      ref="marker"
+      tabindex="0"
+      @keyup.delete="remove"
+    >
+      {{ index }}
+    </div>
+  `,
+  props: {
+    localContext: Object,
+    index: Number,
+    moveTo: Function,
+    remove: Function,
+  },
+  data() {
+    return {
+      grabbing: false,
+    };
+  },
+  mounted() {
+    this.$refs.marker.addEventListener("mousedown", this.onMouseDown);
+  },
+  beforeUnmount() {
+    this.$refs.marker.removeEventListener("mousedown", this.onMouseDown);
+  },
+  methods: {
+    onMouseDown() {
+      this.grabbing = true;
+      document.addEventListener("mouseup", this.onMouseUp);
+      document.addEventListener("mousemove", this.onMouseMove);
+    },
+    onMouseUp() {
+      this.grabbing = false;
+      document.removeEventListener("mousemove", this.onMouseMove);
+    },
+    onMouseMove(event) {
+      const engine2d = this.localContext.viewer.viewer;
+      const { x: cx, y: cy } = engine2d.canvas.getBoundingClientRect();
+      const { x, y } = this.$refs.marker.getBoundingClientRect();
+
+      const { movementX, movementY } = event;
+
+      const position = engine2d.camera.getPosition({
+        x: (x - cx) + movementX,
+        y: (y - cy) + movementY,
+      });
+
+      this.moveTo(position);
+    }
+  },
+};
+
+export default function(viewerId) {
+  const viewer = makeBIMDataViewer({
+    locale: "fr",
+    api: {
+      accessToken: "fZ5CP4g2QQlj2KBXxqdl2jTNokiESIde",
+      cloudId: 11520,
+      projectId: 245620,
+      modelIds: [1173804],
+    },
+    ui: {
+      version: false,
+      bimdataLogo: false,
+    },
+    plugins: {
+      bcf: false,
+      fullscreen: false,
+      measure2d: false,
+      plan: {
+        help: false,
+        modelLoader: "hidden",
+      },
+      "right-angled": false,
+      "window-split": false,
+    }
+  });
+
+  viewer.registerPlugin(PlanAnnotationsPlugin);
+
+  viewer.mount(`#${viewerId}`, "plan");
+}
--- src/.vuepress/viewer/index.js
+++ src/.vuepress/viewer/index.js
@@ -1,24 +1,25 @@
-import simple from "./config/simple.js";
-import pluginUi from "./config/pluginUi.js";
-import windowUI from "./config/windowUI.js";
-import shortcuts from "./config/shortcuts.js";
 import contextMenu from "./config/contextMenu.js";
-import state from "./config/state.js";
 import hubs from "./config/hubs.js";
+import planAnnotations from "./config/planAnnotations.js";
+import pluginUi from "./config/pluginUi.js";
+import shortcuts from "./config/shortcuts.js";
+import simple from "./config/simple.js";
+import state from "./config/state.js";
+import windowUI from "./config/windowUI.js";
 
 const configs = new Map([
-  ["simple", simple],
-  ["pluginUi", pluginUi],
-  ["windowUI", windowUI],
-  ["shortcuts", shortcuts],
   ["contextMenu", contextMenu],
-  ["state", state],
   ["hubs", hubs],
+  ["planAnnotations", planAnnotations],
+  ["pluginUi", pluginUi],
+  ["shortcuts", shortcuts],
+  ["simple", simple],
+  ["state", state],
+  ["windowUI", windowUI],
 ]);
 
-
 async function makeViewer(config, id) {
-  await import(/* webpackIgnore: true */ "https://cdn.jsdelivr.net/npm/@bimdata/viewer@1.9.3");
+  await import(/* webpackIgnore: true */ "https://cdn.jsdelivr.net/npm/@bimdata/viewer@2.0.0-beta.104");
   if (configs.has(config)) {
     return configs.get(config)(id);
   } else {
