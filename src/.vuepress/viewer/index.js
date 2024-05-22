849a7554719a2ec9cc03915b0dd11b5c7bfa4777
Nicolas Richel <nicolas@bimdata.io>
2024-05-22 16:35:35 +0200

    doc(viewer): add ifc 2d/3d annotations example (#71)

--- src/.vuepress/viewer/index.js
+++ src/.vuepress/viewer/index.js
@@ -1,33 +1,33 @@
 import contextMenu from "./config/contextMenu.js";
+import contextPlugins from "./config/contextPlugins.js";
+import globalComponents from "./config/globalComponents.js";
+import guiLayout from "./config/guiLayout.js";
 import hubs from "./config/hubs.js";
+import ifcAnnotations from "./config/ifcAnnotations.js";
 import planAnnotations from "./config/planAnnotations.js";
 import pluginUI from "./config/pluginUI.js";
 import shortcuts from "./config/shortcuts.js";
 import simple from "./config/simple.js";
 import state from "./config/state.js";
 import windowUI from "./config/windowUI.js";
-import defaultConfig from "./config/default.js";
-import guiLayout from "./config/guiLayout.js";
-import contextPlugins from "./config/contextPlugins.js";
-import globalComponents from "./config/globalComponents.js";
 
 const configs = new Map([
   ["contextMenu", contextMenu],
-  ["hubs", hubs],
-  ["guiLayout", guiLayout],
   ["contextPlugins", contextPlugins],
   ["globalComponents", globalComponents],
+  ["guiLayout", guiLayout],
+  ["hubs", hubs],
+  ["ifcAnnotations", ifcAnnotations],
   ["planAnnotations", planAnnotations],
   ["pluginUI", pluginUI],
   ["shortcuts", shortcuts],
   ["simple", simple],
   ["state", state],
   ["windowUI", windowUI],
-  ["default", defaultConfig],
 ]);
 
 async function makeViewer(config, id) {
-  await import(/* webpackIgnore: true */ "https://cdn.jsdelivr.net/npm/@bimdata/viewer@2.1.0-beta.3");
+  await import(/* webpackIgnore: true */ "https://cdn.jsdelivr.net/npm/@bimdata/viewer@2.3.0-beta.12");
   if (configs.has(config)) {
     return configs.get(config)(id);
   } else {
